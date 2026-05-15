109d87d2c12ea3e34e1a16a3efe38554b47c0fcf
samuel.matos <samuel.matos@prof.sc.senac.br>
2026-05-15 19:18:35 -0300

    Correção atividade

---
+++ AulaDesk/src/main/java/com/example/auladesk/MenuController.java
@@ -0,0 +1,17 @@
+package com.example.auladesk;
+
+import javafx.fxml.FXML;
+
+public class MenuController {
+
+
+    @FXML
+    private void onVoltarButtonClick(){
+
+    }
+
+    @FXML
+    private void onCadastroUsuarioButtonClick(){
+
+    }
+}
---
+++ AulaDesk/src/main/java/com/example/auladesk/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.example.auladesk;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UsuarioController {
+
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private void onVoltarButtonClick(){
+
+    }
+
+    @FXML
+    private void onSalvarButtonClick(){
+
+    }
+}
