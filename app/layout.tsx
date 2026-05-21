8ce8961674f1f1be8db6f16bea0fd94b8a8e34d2
samuel.matos <samuel.matos@prof.sc.senac.br>
2026-05-21 19:36:30 -0300

    Tirando context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { AuthProvider } from "./context/AuthContext";
 import StoreProvider from "./redux/StoreProvider";
 
 const geistSans = Geist({
